8e0868a4970fb848a0d284c31241f27487a9e111
WinStuff <enderfoxbg670@gmail.com>
2024-09-05 11:30:09 +0300

    Update Thorium

--- src/Executables/browser.ps1
+++ src/Executables/browser.ps1
@@ -42,11 +42,20 @@ if ($Firefox) {
 
 # Install Thorium browser
 if ($Thorium) {
-	Write-Output "Downloading Thorium..."
-	$installer_link = ((Invoke-RestMethod -Uri "https://api.github.com/repos/Alex313031/Thorium-Win/releases/latest" -Headers @{ "User-Agent" = "Mozilla/5.0" }).assets | Where-Object { $_.name -like "*mini_installer.exe" }).browser_download_url
-	& curl.exe -LSs "https://github.com/Alex313031/Thorium-Win/releases/download/M126.0.6478.251/thorium_Th24_mini_installer.exe" -o "$tempDir\thorium.exe"
-	Start-Process -FilePath "$tempDir\thorium.exe" -WindowStyle Hidden -Wait 2>&1 | Out-Null
-	exit
+    Write-Output "Downloading Thorium..."
+    $installer = ((Invoke-RestMethod -Uri "https://api.github.com/repos/Alex313031/Thorium-Win/releases/latest" -Headers @{ "User-Agent" = "Mozilla/5.0" }).assets | Where-Object { $_.name -like "*mini_installer.exe" }).browser_download_url
+    if (-not $installer) { throw "Installer URL not found" }
+    curl.exe -LSs $installer -o "$tempDir\thorium_mini_installer.exe"
+    $process = Start-Process -FilePath "$tempDir\thorium_mini_installer.exe" -NoNewWindow -PassThru
+    $installer_pid = $process.Id
+    do {
+    Start-Sleep -Seconds 1
+    $installer_running = Get-Process -Id $installer_pid -ErrorAction SilentlyContinue
+    } while ($installer_running)
+    Start-Sleep -Seconds 10
+    Get-Process -Name "thorium" -ErrorAction SilentlyContinue | Stop-Process
+    Write-Output "Thorium has been installed."
+    exit
 }
 
 
